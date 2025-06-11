9d79621eee1b36b9ea3d0706250b13694be56b9d
Breno Dupin <breno.dupin@gmail.com>
2025-06-11 20:16:51 -0300

    improvements on CLI text

--- src/gdt/cli.py
+++ src/gdt/cli.py
@@ -9,38 +9,33 @@ from gdt import __version__
 from pathlib import Path
 import argparse
 
-c_BLUE = "\033[34m"
-c_YELLOW = "\033[33m"
-c_GREEN = "\033[32m"
 c_RESET = "\033[0m"
 
-GDT_BANNER = f"""
-           ╔════════════════════════════════╗
+GDT_BANNER = f"""           ╔════════════════════════════════╗
            ║    ██████╗ ██████╗ ████████╗   ║
            ║   ██╔════╝ ██╔══██╗╚══██╔══╝   ║
            ║   ██║  ███╗██║  ██║   ██║      ║
            ║   ██║   ██║██║  ██║   ██║      ║
            ║   ╚██████╔╝██████╔╝   ██║      ║
            ║    ╚═════╝ ╚═════╝    ╚═╝      ║
-           ║    {c_BLUE}Gene Dictionary Tool{c_RESET}        ║
+           ║    \033[34mGene Dictionary Tool{c_RESET}        ║
            ╚════════════════════════════════╝
-🧬 {c_YELLOW}Standardizing gene names across organelle genomes{c_RESET} 🧬
-                   Version: {c_GREEN}{__version__}{c_RESET}
-"""
+🧬 \033[33mStandardizing gene names across organelle genomes{c_RESET} 🧬
+                   Version: \033[32m{__version__}{c_RESET}"""
 
 
 def cli_run() -> None:
     """Command line interface for the Gene Dictionary Tool (gdt)"""
 
-    # Global parser for the command line interface
+    # Global parser to add debug, log, and quiet flags to all subcommands
     gp = argparse.ArgumentParser(add_help=False)
     gp.add_argument(
         "--debug",
         required=False,
         default=False,
         action="store_true",
-        help="Enable TRACE level in file log, and DEBUG on console. "
-        "Default: False (DEBUG level on file and INFO on console)",
+        help="Enable TRACE level in file, and DEBUG on console. "
+        "Default: DEBUG level on file and INFO on console.",
     )
 
     gp.add_argument(
@@ -56,26 +51,30 @@ def cli_run() -> None:
         required=False,
         default=False,
         action="store_true",
-        help="Suppress console output. " "Default: False (console output enabled)",
+        help="Suppress console output. Default: console output enabled.",
     )
 
     parser = argparse.ArgumentParser(
-        description="Gene Dictionary Tool (gdt) command line interface",
+        description=GDT_BANNER,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
         parents=[gp],
+        epilog=f"Source ~ \033[32mhttps://gitlab.com/brenodupin/gdt{c_RESET}",
     )
     parser.add_argument(
         "--version",
         action="version",
-        version=f"%(prog)s {__version__}",
-        help="Show the version of the gdt package",
+        version=f"gdt {__version__}",
+        help="Show the version of the gdt package.",
     )
     subparsers = parser.add_subparsers(dest="command", help="Commands")
     subparsers.required = True
 
     filter_parser = subparsers.add_parser(
         "filter",
-        help="Filter GFF3 files index in a TSV file",  # Brief help for main command list
-        description="Filter GFF3 files that are indexed in a TSV file into AN_missing_dbxref or AN_missing_gene_dict categories",
+        help="Filter GFF3 files indexed through TSV file.",
+        description="Filter GFF3 files that are indexed via TSV file, "
+        "creating AN_missing_dbxref.txt and/or AN_missing_gene_dict.txt "
+        "based on the provided GDT file.",
         parents=[gp],
     )
     filter_parser.add_argument("--tsv", required=True, help="TSV file to filter")
@@ -90,15 +89,16 @@ def cli_run() -> None:
         "--gdt",
         required=False,
         default=False,
+        type=str,
         help="GDT file to use for filtering. "
-        "If not provided, an empty gene_dict will be used.",
+        "If not provided, an empty GeneDict (i.e GDT file) will be used.",
     )
     filter_parser.add_argument(
         "--keep-orfs",
         required=False,
         default=False,
         action="store_true",
-        help="Don't discard ORFs. Default: False (discard ORFs)",
+        help="Keep ORFs. Default: exclude ORFs",
     )
     filter_parser.add_argument(
         "--workers",
@@ -119,38 +119,42 @@ def cli_run() -> None:
         required=False,
         default=gff3_utils.QS_GENE_TRNA_RRNA,
         type=str,
-        help="Pandas query string to filter GFF3 files. "
+        help="Query string that pandas filter features in GFF. "
         f"Default: '{gff3_utils.QS_GENE_TRNA_RRNA}'",
     )
 
     stripped_parser = subparsers.add_parser(
-        "stripped", help="Create a stripped GDT version of a GDT file", parents=[gp]
+        "stripped",
+        help="Create a stripped version of a GDT file",
+        description="Filter GeneGeneric's (#gn) and Dbxref's (#dx) out of a GDT file, "
+        "keeping only GeneDescription (#gd) entries and its metadata. ",
+        parents=[gp],
     )
     stripped_parser.add_argument(
-        "--gdt_in", "-gin", required=True, help="Input GDT file to strip"
+        "--gdt_in", "-gin", required=True, help="Input GDT file to strip."
     )
     stripped_parser.add_argument(
-        "--gdt_out", "-gout", required=True, help="Output GDT file to write"
+        "--gdt_out", "-gout", required=True, help="New GDT file to create."
     )
     stripped_parser.add_argument(
         "--overwrite",
         required=False,
         default=False,
         action="store_true",
-        help="Overwrite output file if it exists. Default: False",
+        help="Overwrite output file, if it already exists. Default: False",
     )
 
-    test_parser = subparsers.add_parser("test", help="Test command", parents=[gp])
+    subparsers.add_parser("test", help="Test command", parents=[gp])
     args = parser.parse_args()
 
     if not args.quiet:
         print(GDT_BANNER)
 
     log = logger_setup.setup_logger(args.debug, args.log, args.quiet)
-    log.debug("CLI run called.")
-    log.debug(f"exec path: {Path().resolve()}")
-    log.debug(f"cli  path: {Path(__file__)}")
-    log.debug(f"args: {args}")
+    log.trace("CLI run called.")
+    log.trace(f"exec path: {Path().resolve()}")
+    log.trace(f"cli  path: {Path(__file__)}")
+    log.trace(f"args: {args}")
 
     if args.command == "filter":
         args.tsv = Path(args.tsv).resolve()
@@ -175,28 +179,22 @@ def cli_run() -> None:
             args.query_string,
         )
 
-    elif args.command == "write":
-        log.info(f"Write command: gdt: {args.gdt} | out: {args.out}")
-        gd = gene_dict_impl.create_gene_dict(args.gdt, max_an_sources=0)
-        gene_dict_impl.write_gdt_file(gd, args.out, overwrite=True)
-
-    elif args.command == "test":
-        log.info(f"Test command: {args}")
-        pass
-
     elif args.command == "stripped":
         log.info(
-            f"Stripped command: gdt_in: {args.gdt_in} | gdt_out: {args.gdt_out} | overwrite: {args.overwrite}"
+            f"Stripped command: gdt_in: {args.gdt_in} | "
+            f"gdt_out: {args.gdt_out} | "
+            f"overwrite: {args.overwrite}"
         )
         args.gdt_in = Path(args.gdt_in).resolve()
         args.gdt_out = Path(args.gdt_out).resolve()
 
         if args.gdt_in.exists():
-            log.debug(f"Input GDT file: {args.gdt_in}")
-            log.debug(f"Output GDT file: {args.gdt_out}")
             gene_dict_impl.create_stripped_gdt(
                 args.gdt_in, args.gdt_out, overwrite=args.overwrite
             )
         else:
             log.error(f"Input GDT file does not exist: {args.gdt_in}")
             raise FileNotFoundError(f"GDT file not found: {args.gdt_in}")
+
+    elif args.command == "test":
+        log.info(f"Test command: {args}")

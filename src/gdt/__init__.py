9c0b7f9dfd31af56ff2946fbc2bd6250ceea60c9
brenodupin <breno.dupin@gmail.com>
2025-06-25 18:36:34 -0300

    more tests + v0.2.6

--- src/gdt/__init__.py
+++ src/gdt/__init__.py
@@ -5,7 +5,7 @@ This package provides tools for working with gene dictionaries, including
 GFF3 file parsing, gene dictionary manipulation, and logging setup.
 """
 
-__version__ = "0.2.5"
+__version__ = "0.2.6"
 
 from .gdt_impl import (
     DbxrefGeneID,
